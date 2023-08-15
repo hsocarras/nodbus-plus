f89fd8be1072b5c69e8106ebeb6693fe3200938b
Hector E. Socarras <hsocarras1987@gmail.com>
2023-08-15 17:23:10 +0200

    docs fix 1

--- docs/conf.py
+++ docs/conf.py
@@ -4,6 +4,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import os
+import sys
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
