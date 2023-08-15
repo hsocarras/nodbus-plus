83fa2616d80b61e8401728a116c33aaff7825038
Hector E. Socarras <hsocarras1987@gmail.com>
2023-08-15 16:27:04 +0200

    change on conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,6 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-import sphinx_rtd_theme
-html_theme = 'sphinx_rtd_theme'
+#import sphinx_rtd_theme
+#html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
