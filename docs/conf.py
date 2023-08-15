bc8c9ea8e5d8f5193dd5a2b98e353bce9b10078c
Hector E. Socarras <hsocarras1987@gmail.com>
2023-08-15 17:39:02 +0200

    doc fix2

--- docs/conf.py
+++ docs/conf.py
@@ -30,17 +30,11 @@ exclude_patterns = []
 # on_rtd is whether we are on readthedocs.org
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    try:
-        import sphinx_rtd_theme
-        html_theme = 'sphinx_rtd_theme'
-        html_theme_path = [sphinx_rtd_theme.get_html_theme_path(), '.']
-    except:
-        html_theme = 'default'
-        html_theme_path = ['.']
-else:
-    html_theme_path = ['.']
+if not on_rtd:  # only import and set the theme if we're building docs locally    
+    import sphinx_rtd_theme
+        
+
 
 #import sphinx_rtd_theme
-#html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['static']
